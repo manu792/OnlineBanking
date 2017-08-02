da7b41ea96ea8cb22defd49c83d33abcb89fe913
manuel.roman <manuel.romansoto@fiserv.com>
2017-08-02 12:20:13 -0600

    modified and added new property for code behind file

--- OnlineBanking/BusinessForm.ascx.cs
+++ OnlineBanking/BusinessForm.ascx.cs
@@ -12,30 +12,12 @@ namespace OnlineBanking
     public partial class BusinessForm : PortalModuleControl
     {
         private static readonly LogHelper logger = new LogHelper(LogSystem.CreateTypeContextLogger());
+        
+        public string greetingLabel = "Hi Manuel";
 
-        #region Component Settings
-        /*
-		 * An example of a consuming a configurable Portal service.  The type of this property can be any type
-		 * (it does not have to specifically be object).  The WellKnownComponent = true property in the attribute
-		 * informs portal to display a dropdown of services of this type in the module editor.
-		 */
-        //[ComponentSetting("Configurable Service", "Services", WellKnownComponent = true)]
-        //public object ConfigurableService { get; set; }
+        [ComponentSetting("Configurable Service", "Services", WellKnownComponent = true)]
+        public string GreetingLabel { get { return greetingLabel; } set { greetingLabel = value; } }
 
-        /*
-		 * An example of consuming a configurable setting.  The type of this property can be any value type
-		 * or any reference type that can be parsed or serialized.  Note:  Any string that is displayed to
-		 * the end user should also have an IDS.ComponentModel.Localizable attribute
-		 */
-        //[ComponentSetting("Configurable Property", "Settings")]
-        //public string ConfigurableProperty { get; set; }
-        #endregion Component Settings
-
-        /*
-		 * Delete this method and the accompanying .resx and .es.resx files to disable Spanish localization.
-		 * Otherwise, just delete this comment.
-		 * See http://wiki.idsus.com:8081/download/attachments/13893993/Command+Spanish+Design.docx
-		 */
         public override void LoadLanguageDefaults(System.Globalization.CultureInfo culture)
         {
             base.LoadLanguageDefaults(culture);
